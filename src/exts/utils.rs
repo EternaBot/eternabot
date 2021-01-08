a05bedead070ba94a89dc568026aaadf621bb411
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-08 20:23:52 +0200

    Fix Rustfmt linting

--- src/exts/utils.rs
+++ src/exts/utils.rs
@@ -1,9 +1,9 @@
-use serenity::prelude::Context;
-use serenity::model::prelude::Message;
 use serenity::framework::standard::{
-    CommandResult,
     macros::{command, group},
+    CommandResult,
 };
+use serenity::model::prelude::Message;
+use serenity::prelude::Context;
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
+use serenity::{framework::standard::StandardFramework, prelude::Client};
 use std::env;
 use std::ffi::OsString;
-use serenity::{
-    prelude::Client,
-    framework::standard::StandardFramework,
-};
 
 use exts::*;
 
@@ -22,23 +19,26 @@ async fn main() {
     info!("Logger setup complete...");
 
     // Setup Sentry
-    let _guard = sentry::init(env::var_os("SENTRY_DNS")
-        .unwrap_or(OsString::new()).into_string().unwrap());
+    let _guard = sentry::init(
+        env::var_os("SENTRY_DNS")
+            .unwrap_or(OsString::new())
+            .into_string()
+            .unwrap(),
+    );
     info!("Sentry setup complete...");
-    
+
     // Gather token from environment
-    let token: String = env::var("BOT_TOKEN")
-        .expect("Discord bot token required");
+    let token: String = env::var("BOT_TOKEN").expect("Discord bot token required");
 
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!"))
         .group(&utils::UTILS_GROUP);
-    
+
     let mut client = Client::builder(token)
         .framework(framework)
         .await
         .expect("Error while creating client");
-    
+
     info!("Starting EternaBot...");
     if let Err(why) = client.start_autosharded().await {
         error!("Client error: {:?}", why);
