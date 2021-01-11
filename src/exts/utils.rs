c64ff4b27a4319751504cdcf717a9974ed7ae796
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-11 14:05:35 +0200

    Implement advanced ping command

--- src/exts/utils.rs
+++ src/exts/utils.rs
@@ -1,13 +1,59 @@
-use serenity::framework::standard::{
-    macros::{command, group},
-    CommandResult,
+use serenity::{
+    client::bridge::gateway::ShardId,
+    framework::standard::{
+        macros::{command, group},
+        CommandResult,
+    },
+    model::prelude::Message,
+    prelude::Context,
+    utils::Colour,
 };
-use serenity::model::prelude::Message;
-use serenity::prelude::Context;
+
+use crate::typemap_keys::ShardManagerContainer;
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(&ctx.http, "Pong!").await?;
+    // First we need to get shard manager from client data.
+    let data = ctx.data.read().await;
+    let shard_manager = data.get::<ShardManagerContainer>()
+        .expect("Unable to get shard manager");
+    
+    // After we have shard manager, we must get runners.
+    let manager = shard_manager.lock().await;
+    let runners = manager.runners.lock().await;
+
+    // Now we can get specific shard runner.
+    let runner = match runners.get(&ShardId(ctx.shard_id)) {
+        Some(r) => r,
+        None => {
+            msg.reply(ctx, "Unable to get guild's shard information.").await?;
+
+            return Ok(());
+        }
+    };
+    
+    // As now we also have shard information, we can get latency.
+    let latency = match runner.latency {
+        Some(l) => l.as_millis().to_string(),
+        None => String::from("?")
+    };
+
+    msg.channel_id.send_message(ctx, |m| {
+        m.embed(|e| {
+            e.title("Pong!");
+            e.field("Latency", &format!("{:?}**ms**", latency), true);
+            e.footer(|f| {
+                f.text(&format!("Shard {:?}/{:?}", ctx.shard_id, runners.len()));
+
+                f
+            });
+            e.colour(Colour::DARK_GREEN);
+
+            e
+        });
+
+        m
+    }).await?;
 
     Ok(())
 }
