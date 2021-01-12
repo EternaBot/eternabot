7abf10683b56fd9ff106108b00ca3652e3ff8444
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-12 13:55:37 +0200

    Implement PostgreSQL connecting and shutdown

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ extern crate pretty_env_logger;
 extern crate log;
 
 use serenity::{framework::standard::StandardFramework, prelude::Client};
+use sqlx::postgres::PgPoolOptions;
 use std::env;
 use std::ffi::OsString;
 use std::path::Path;
+use tokio::signal::unix::{signal, SignalKind};
 
 use exts::*;
 use handlers::handler::Handler;
@@ -35,7 +37,15 @@ async fn main() {
     info!("Sentry setup complete...");
 
     // Gather token from environment
-    let token: String = env::var("BOT_TOKEN").expect("Discord bot token required");
+    let token = env::var("BOT_TOKEN").expect("Discord bot token required");
+    let database_url = env::var("DATABASE_URL").expect("Database URL required");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(96)
+        .min_connections(50)
+        .connect(database_url.as_str())
+        .await
+        .expect("Can't connect to PostgreSQL DB");
 
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("!"))
@@ -50,7 +60,18 @@ async fn main() {
     {
         let mut data = client.data.write().await;
         data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+        data.insert::<PostgresPool>(pool.clone());
     }
+
+    let shard_manager = client.shard_manager.clone();
+    tokio::spawn(async move {
+        let mut stream = signal(SignalKind::interrupt()).expect("Unable to get SIGINT signal");
+        stream.recv().await;
+        info!("Got SIGTERM signal...");
+        info!("Shutting down...");
+        shard_manager.lock().await.shutdown_all().await;
+        pool.close().await;
+    });
 
     info!("Starting EternaBot...");
     if let Err(why) = client.start_autosharded().await {
