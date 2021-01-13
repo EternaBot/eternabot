69c12f6ac686f9f0ed5803fe2dfd0c0702016876
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-13 16:46:01 +0200

    Add guild syncing to handlers

--- src/handlers/handler.rs
+++ src/handlers/handler.rs
@@ -1,8 +1,10 @@
 use serenity::{
     async_trait,
-    model::id::GuildId,
+    model::{guild::Guild, id::GuildId},
     prelude::{Context, EventHandler},
 };
+
+use crate::utils::guild::insert_guild;
 
 use super::ready;
 
@@ -12,5 +14,9 @@ pub struct Handler;
 impl EventHandler for Handler {
     async fn cache_ready(&self, ctx: Context, _guilds: Vec<GuildId>) {
         ready::cache_ready(ctx).await;
+    }
+
+    async fn guild_create(&self, ctx: Context, guild: Guild, _is_new: bool) {
+        insert_guild(ctx, guild).await;
     }
 }
