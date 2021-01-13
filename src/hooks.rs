5cd13c4e2ace6a9bb4200251f0300d0d8da8f5bd
ks129 <45097959+ks129@users.noreply.github.com>
2021-01-13 08:41:47 +0200

    Add after command execution hook

---
+++ src/hooks.rs
@@ -0,0 +1,20 @@
+use serenity::{
+    framework::standard::{
+        macros::hook,
+        CommandResult
+    },
+    prelude::Context,
+    model::prelude::Message,
+};
+
+#[hook]
+pub async fn after(_ctx: &Context, msg: &Message, command_name: &str, command_result: CommandResult) {
+    match command_result {
+        Ok(()) => {
+            info!("Processed command '{}' by {} ({})", command_name, msg.author.tag(), msg.author.id);
+        },
+        Err(why) => {
+            error!("Command '{}' by {} ({}) returned with error: {}", command_name, msg.author.tag(), msg.author.id, why);
+        }
+    }
+}
